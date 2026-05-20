3410bd1adac2973212147a17fbcad987a867dc07
Lionel FÉLICITÉ <deogracia@users.noreply.github.com>
2026-05-21 00:10:41 +0200

    refacto main + tests unitaire

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -19,46 +19,21 @@ import (
 	"github.com/spf13/viper"
 )
 
-func main() {
-	config.LoadConfig()
-
-	if viper.GetString("app.secret_key") == "" {
-		log.Fatal("🛑 ERREUR FATALE : la clé de configuration 'app.secret_key' est manquante. Le serveur refuse de démarrer pour des raisons de sécurité.")
-	}
-
-	database.Connect()
-	services.InitDefaultSettings()
-
+// setupRouter configure Gin en fonction de l'environnement
+func setupRouter(env string) *gin.Engine {
 	r := gin.Default()
-	// 0. Gestion de l'environnement (Prduction par défaut)
-	env := os.Getenv("APP_ENV")
-	if env == "" {
-		env = "production"
-	}
 
 	if env == "development" {
-		fmt.Println("🚀 Démarrage en mode DÉVELOPPEMENT (lecture des templates sur disque)")
-		// 1a. [DEV] CHARGEMENT DES TEMPLATES HTML ---
-		// On utilise os.DirFS pour créer un système de fichiers virtuel pointant sur le dossier physique
+		fmt.Println("🚀 Mode DÉVELOPPEMENT (lecture sur disque)")
 		templ := template.Must(template.ParseFS(os.DirFS("templates"), "*.html", "partials/*.html"))
 		r.SetHTMLTemplate(templ)
-
 		r.StaticFile("/favicon.ico", "static/favicon.ico")
-
 	} else {
-
-		gin.SetMode(gin.ReleaseMode) // on est en prod
-
-		fmt.Println("📦 Démarrage en mode PRODUCTION (lecture des templates depuis l'exécutable)")
-		// 1b. [PROD] CHARGEMENT DES TEMPLATES HTML ---
-		// On charge d'abord les pages depuis l'exécutable
+		fmt.Println("📦 Mode PRODUCTION (lecture depuis l'exécutable)")
+		gin.SetMode(gin.ReleaseMode)
 		templ := template.Must(template.ParseFS(templates.TemplateFS, "*.html", "partials/*.html"))
-		// On y ajoute (fusionne) les fragments du sous-dossier
-		template.Must(templ.ParseGlob("templates/partials/*.html"))
 		r.SetHTMLTemplate(templ)
-
 		r.StaticFileFS("/favicon.ico", "favicon.ico", http.FS(static.StaticFS))
-
 	}
 
 	// 1. Le Gatekeeper : Force le setup si la base est vierge
@@ -143,6 +118,143 @@ func main() {
 		api.DELETE("/limbs/:id", handlers.DeleteLimb)
 
 	}
+
+	return r
+}
+
+func main() {
+	config.LoadConfig()
+
+	if viper.GetString("app.secret_key") == "" {
+		log.Fatal("🛑 ERREUR FATALE : la clé de configuration 'app.secret_key' est manquante. Le serveur refuse de démarrer pour des raisons de sécurité.")
+	}
+
+	database.Connect()
+	services.InitDefaultSettings()
+
+	// r := gin.Default()
+	// 0. Gestion de l'environnement (Prduction par défaut)
+	env := os.Getenv("APP_ENV")
+	switch env {
+	case "", "development", "production":
+		log.Println("APP_ENV a une valeur autorisée!")
+	default:
+		log.Fatalf("🛑 ERREUR FATALE : Environement de démarage %s non pris en charge. L'application s'arrête.", env)
+	}
+
+	if env == "" {
+		env = "production"
+	}
+
+	r := setupRouter(env)
+
+	//	if env == "development" {
+	//		fmt.Println("🚀 Démarrage en mode DÉVELOPPEMENT (lecture des templates sur disque)")
+	//		// 1a. [DEV] CHARGEMENT DES TEMPLATES HTML ---
+	//		// On utilise os.DirFS pour créer un système de fichiers virtuel pointant sur le dossier physique
+	//		templ := template.Must(template.ParseFS(os.DirFS("templates"), "*.html", "partials/*.html"))
+	//		r.SetHTMLTemplate(templ)
+	//
+	//		r.StaticFile("/favicon.ico", "static/favicon.ico")
+	//
+	//	} else {
+	//
+	//		gin.SetMode(gin.ReleaseMode) // on est en prod
+	//
+	//		fmt.Println("📦 Démarrage en mode PRODUCTION (lecture des templates depuis l'exécutable)")
+	//		// 1b. [PROD] CHARGEMENT DES TEMPLATES HTML ---
+	//		// On charge d'abord les pages depuis l'exécutable
+	//		templ := template.Must(template.ParseFS(templates.TemplateFS, "*.html", "partials/*.html"))
+	//		// On y ajoute (fusionne) les fragments du sous-dossier
+	//		template.Must(templ.ParseGlob("templates/partials/*.html"))
+	//		r.SetHTMLTemplate(templ)
+	//
+	//		r.StaticFileFS("/favicon.ico", "favicon.ico", http.FS(static.StaticFS))
+	//
+	//	}
+
+	//	// 1. Le Gatekeeper : Force le setup si la base est vierge
+	//	r.Use(middleware.EnsureSetup())
+	//
+	//	// ==========================================
+	//	// 🌐 PARTIE FRONT-END (HTML)
+	//	// ==========================================
+	//
+	//	// Route publique pour afficher le formulaire de connexion
+	//	r.GET("/login", func(c *gin.Context) {
+	//		c.HTML(http.StatusOK, "login.html", gin.H{"titre": "Connexion - Toxophilus"})
+	//	})
+	//
+	//	// Groupe pour les pages HTML privées (le Dashboard)
+	//	web := r.Group("/")
+	//	web.Use(middleware.AuthRequired()) // On recycle notre middleware JWT !
+	//	{
+	//		web.GET("/", handlers.GetDashboardPage)
+	//
+	//		// List des membres
+	//		web.GET("/members", handlers.GetMembersPage)
+	//
+	//		// Page pour modifier un membre
+	//		web.GET("/members/edit/:id", handlers.GetMemberEditPage)
+	//
+	//		// Page des archives (Membres supprimés)
+	//		web.GET("/members/archives", handlers.GetMemberArchivesPage)
+	//
+	//		// les routes spécifiques aux matériel
+	//		web.GET("/equipement", handlers.GetEquipementPage)
+	//		web.GET("/equipement/edit/riser/:id", handlers.GetEditRiserPage)
+	//		web.GET("/equipement/edit/limb/:id", handlers.GetEditLimbPage)
+	//
+	//	}
+	//
+	//	// 2. Routes de Configuration Initiale
+	//	r.GET("/setup", handlers.GetSetupPage)
+	//	r.POST("/setup/process", handlers.ProcessSetup)
+	//
+	//	// ==========================================
+	//	// ⚙️ PARTIE BACK-END (API REST JSON)
+	//	// ==========================================
+	//
+	//	// 3. Routes Publiques
+	//	r.POST("/api/login", handlers.LoginHandler)
+	//	r.POST("/api/logout", handlers.LogoutHandler)
+	//
+	//	// 4. Routes Protégées
+	//	api := r.Group("/api")
+	//	api.Use(middleware.AuthRequired())
+	//	{
+	//		// 	api.GET("/risers", handlers.ListRisers)
+	//
+	//		// on teste que ça se goupille bien.
+	//		api.GET("/me", func(ctx *gin.Context) {
+	//			userID, _ := ctx.Get("userID")
+	//			ctx.JSON(200, gin.H{"message": "Accès Autorisé", "ton_id_utilisateur": userID})
+	//
+	//		})
+	//
+	//		// Gestion des membres
+	//		api.GET("/members", handlers.ListMembers)
+	//		api.POST("/members", handlers.CreateMember)
+	//		api.PUT("/members/:id", handlers.UpdateMember)
+	//		api.DELETE("/members/:id", handlers.DeleteMember)
+	//		api.GET("/members/:id/export", handlers.ExportMemberData)
+	//		api.DELETE("/members/:id/hard", handlers.HardDeleteMember)
+	//		api.PUT("/members/:id/reactivate", handlers.ReactivateMember)
+	//
+	//		// Gestion équipement
+	//		//  Poignée
+	//		api.GET("/risers", handlers.ListRisers)
+	//		api.POST("/risers", handlers.CreateRiser)
+	//		api.PUT("/risers/:id", handlers.UpdateRiser)
+	//		api.DELETE("/risers/:id", handlers.DeleteRiser)
+	//
+	//		// Branches
+	//		api.GET("/limbs", handlers.ListLimbs)
+	//		api.POST("/limbs", handlers.CreateLimb)
+	//		api.PUT("/limbs/:id", handlers.UpdateLimb)
+	//		api.DELETE("/limbs/:id", handlers.DeleteLimb)
+	//
+	//	}
 
 	port := viper.GetString("app.port")
 	if port == "" {
---
+++ cmd/server/main_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/deogracia/toxophilus/database"
+	"github.com/deogracia/toxophilus/models"
+	"github.com/gin-gonic/gin"
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+)
+
+// setupMockDB crée une base SQLite en mémoire pour éviter que les routes ne paniquent
+func setupMockDB() {
+	db, _ := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	database.DB = db
+
+	// on force la migration que des models nécessaires pour les tests
+	database.DB.AutoMigrate(&models.User{}, &models.Setting{})
+	// on créé un utilisateur
+	database.DB.Create(&models.User{
+		Email:    "test@example.test",
+		Password: "password",
+	})
+}
+
+func TestEnvironments(t *testing.T) {
+	// ATTENTION ASTUCE :
+	// Quand on lance `go test ./cmd/server`, le dossier de travail courant devient `cmd/server`.
+	// Pour que `os.DirFS("templates")` fonctionne en mode dev, on doit remonter à la racine du projet.
+	os.Chdir("../..")
+
+	setupMockDB()
+	gin.SetMode(gin.TestMode)
+
+	// On va tester les deux modes d'un seul coup
+	tests := []struct {
+		name string
+		env  string
+	}{
+		{"Mode Développement", "development"},
+		{"Mode Production", "production"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// 1. Initialisation du routeur dans l'environnement donné
+			r := setupRouter(tt.env)
+
+			// 2. Test du Favicon
+			reqFavicon, _ := http.NewRequest("GET", "/favicon.ico", nil)
+			wFavicon := httptest.NewRecorder()
+			r.ServeHTTP(wFavicon, reqFavicon)
+
+			if wFavicon.Code != http.StatusOK {
+				t.Errorf("Attendu 200 OK pour favicon en %s, obtenu %d", tt.env, wFavicon.Code)
+			}
+
+			// 3. Test des Templates (Si /login renvoie 200, c'est que les templates ont bien été parsés)
+			reqLogin, _ := http.NewRequest("GET", "/login", nil)
+			wLogin := httptest.NewRecorder()
+			r.ServeHTTP(wLogin, reqLogin)
+
+			if wLogin.Code != http.StatusOK {
+				t.Errorf("Attendu 200 OK pour la page login en %s, obtenu %d", tt.env, wLogin.Code)
+			}
+		})
+	}
+}
